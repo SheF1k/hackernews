1e7c70210ee75138ede60078c6fa5102d0b9b4da
SheF1k <kissiarule@gmail.com>
2020-11-25 18:04:02 +0200

    add prop types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -149,6 +150,17 @@ const Search = ({ value, onChange, onSubmit, children }) => (
   </form>
 );
 
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Search.defaultProps = {
+  value: '',
+};
+
 const Table = ({ list, onDismiss }) => (
   <div className="table">
     {list.map((item) => (
@@ -172,10 +184,33 @@ const Table = ({ list, onDismiss }) => (
   </div>
 );
 
-const Button = ({ onClick, className = '', children }) => (
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    }),
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
+
+const Button = ({ onClick, className, children }) => (
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+  className: '',
+};
 
 export default App;
